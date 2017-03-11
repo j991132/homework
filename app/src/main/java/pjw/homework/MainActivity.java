e24fed36b4cb8b0ba10880e3f332b0a508e4ea7d
j991132 <j991132@naver.com>
2017-03-11 14:01:04 +0900

    2017.03.11 백키 두번 눌러 종료

--- app/src/main/java/pjw/homework/MainActivity.java
+++ app/src/main/java/pjw/homework/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -15,21 +17,25 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     //전역변수선언
     Button addBtn;
+    Button resetBtn;
     Button[] btn = new Button[20];
     LinearLayout layout;
     Context context;
     ScrollView sv;
     int count = 0;
     String id = null;
-    String[] subject = new String[20];
+    String[] subject = new String[20];  //배열변수
     int num = 0;
     int[] studentnumber = new int[20];
     String Id;
     int[] activitynumber = new int[20];
     int i=0;
-    String[] name= new String[20];
-    int[] studentnum = new int[20];
+    String[] name= new String[20];    
+    int[] studentnum = new int[20];      
     int[] actnum = new int[20];
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,9 +43,17 @@ public class MainActivity extends AppCompatActivity {
 
 
         addBtn = (Button) findViewById(R.id.addBtn);
+        resetBtn = (Button)findViewById(R.id.reset);
         //sv = (ScrollView)findViewById(R.id.scrollView);
         layout = (LinearLayout) findViewById(R.id.layout);
         context = this;
+//초기화버튼 눌렀을때
+        resetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //초기화 명령
+            }
+        });
 //추가버튼 눌렀을때
         addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -204,5 +218,64 @@ public class MainActivity extends AppCompatActivity {
 }
 
         }
+    // Back의 상태값을 저장하기 위한 변수
+    boolean m_close_flag = false;
+
+
+    // 일정 시간 후 상태값을 초기화하기 위한 핸들러
+    Handler m_close_handler = new Handler() {
+        public void handleMessage(Message msg) {
+            m_close_flag = false;
+        }
+    };
+
+
+
+    // ... 관련 없는 코드 생략 ...
+
+
+
+    // Back 키가 터치되면 호출되는 메소드
+    public void onBackPressed ()
+    {
+
+        // m_close_flag 가 false 이면 첫번째로 키가 눌린 것이다.
+        if(m_close_flag == false) { // Back 키가 첫번째로 눌린 경우
+
+
+
+            // 안내 메세지를 토스트로 출력한다.
+            Toast.makeText(this, "취소키를 빨리 한번 더 누르시면 종료됩니다.", Toast.LENGTH_LONG).show();
+
+
+
+            // 상태값 변경
+            m_close_flag = true;
+
+
+
+            // 핸들러를 이용하여 3초 후에 0번 메세지를 전송하도록 설정한다.
+            m_close_handler.sendEmptyMessageDelayed(0, 3000);
+
+
+        } else { // Back 키가 3초 내에 연달아서 두번 눌린 경우
+
+
+
+            // 액티비티를 종료하는 상위 클래스의 onBackPressed 메소드를 호출한다.
+            super.onBackPressed();
+        }
+    }
+
+
+
+    protected void onStop()
+    {
+        super.onStop();
+
+        // 핸드러에 등록된 0번 메세지를 모두 지운다.
+
+        m_close_handler.removeMessages(0);
+    }
     }
 
